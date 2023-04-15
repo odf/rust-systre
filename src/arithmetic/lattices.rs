508640eb4d72b55faceac9b20ca527a7ccf3b14e
odf <olaf.delgado@gmail.com>
2023-04-15 12:52:22 +1000

    add more lattice manipulation code (untested)

--- src/arithmetic/lattices.rs
+++ src/arithmetic/lattices.rs
@@ -1,12 +1,16 @@
 use std::ops::{Add, Sub, Mul, Neg};
+use std::cmp::Ordering;
 
-use num_traits::{Zero, One, Signed};
+use num_traits::{Signed, abs};
+
+use super::linear_algebra::{extend_basis, Scalar};
 
 
 pub trait Coord:
-    Clone + PartialOrd + Zero + One + Signed
+    Clone + PartialOrd + Signed + Scalar
     + for <'a> Add<&'a Self, Output=Self>
 {
+    fn round(&self) -> Self;
     fn div_rounded(&self, other: &Self) -> Self;
 }
 
@@ -16,6 +20,71 @@ pub trait CoordPtr<T>:
     + Add<Output=T> + Sub<Output=T> + Sub<T, Output=T> + Neg<Output=T>
     + Mul<Output=T>
 {
+}
+
+
+pub fn shift_for_dirichlet_domain<T>(p: &[T], vs: &[Vec<T>], epsilon: T)
+    -> Vec<T>
+    where T: Coord, for <'a> &'a T: CoordPtr<T>
+{
+    let pos = p.to_vec();
+    let mut shift = vec![T::zero(); p.len()];
+    let mut proceed = true;
+
+    while proceed {
+        proceed = false;
+
+        for v in vs {
+            let t = dot(&add(&pos, &shift), &shift) / dot(v, v);
+            let t2 = &t + &t;
+            if t2 < -T::one() || t2 > T::one() + &epsilon {
+                shift = sub(&shift, &mul(v, &t.round()));
+                proceed = true;
+            }
+        }
+    }
+
+    shift
+}
+
+
+pub fn reduced_lattice_basis<T>(vs: &[Vec<T>], epsilon: T)
+    -> Option<Vec<Vec<T>>>
+    where T: Coord, for <'a> &'a T: CoordPtr<T>
+{
+    let dim = vs[0].len();
+    let mut a: Vec<Vec<T>> = vec![];
+    let mut i = 0;
+
+    let mut ws = dirichlet_vectors(vs, epsilon.clone());
+    ws.sort_by(|v, w| {
+        let d = dot(v, v).partial_cmp(&dot(w, w)).unwrap();
+        if d <= Ordering::Equal {
+            d
+        } else {
+            positive_direction(v).partial_cmp(&positive_direction(w)).unwrap()
+        }
+    });
+
+    for w in &ws {
+        let w: Vec<_> = w.iter().map(|x|
+                if &abs(x.clone()) < &epsilon { T::zero() } else { x.clone() }
+            ).collect();
+        a[i] = if i > 0 && dot(&a[0], &w).is_positive() {
+            neg(&w)
+        } else {
+            w.clone()
+        };
+
+        if rank(&a) > i {
+            i += 1;
+            if i >= dim {
+                return Some(a);
+            }
+        }
+    }
+
+    None
 }
 
 
@@ -108,6 +177,17 @@ fn selling_reduced<T>(u: &[T], v: &[T], w: &[T], epsilon: T)
 }
 
 
+fn rank<T>(vs: &[Vec<T>]) -> usize
+    where T: Coord, for <'a> &'a T: CoordPtr<T>
+{
+    let mut basis = vec![];
+    for v in vs {
+        extend_basis(&v, &mut basis);
+    }
+    basis.len()
+}
+
+
 fn dot<T>(v: &[T], w: &[T]) -> T
     where T: Coord, for <'a> &'a T: CoordPtr<T>
 {
@@ -127,8 +207,36 @@ fn positive_direction<T>(v: &[T]) -> Vec<T>
 {
     if let Some(x) = v.iter().find(|x| !x.is_zero()) {
         if x.is_negative() {
-            return v.iter().map(|x| -x).collect();
+            return neg(v);
         }
     }
     v.to_vec()
+}
+
+
+fn neg<T>(v: &[T]) -> Vec<T>
+    where T: Coord, for <'a> &'a T: CoordPtr<T>
+{
+    v.iter().map(|x| -x).collect()
+}
+
+
+fn add<T>(v: &[T], w: &[T]) -> Vec<T>
+    where T: Coord, for <'a> &'a T: CoordPtr<T>
+{
+    (0..v.len()).map(|i| &v[i] + &w[i]).collect()
+}
+
+
+fn sub<T>(v: &[T], w: &[T]) -> Vec<T>
+    where T: Coord, for <'a> &'a T: CoordPtr<T>
+{
+    (0..v.len()).map(|i| &v[i] - &w[i]).collect()
+}
+
+
+fn mul<T>(v: &[T], t: &T) -> Vec<T>
+    where T: Coord, for <'a> &'a T: CoordPtr<T>
+{
+    (0..v.len()).map(|i| &v[i] * t).collect()
 }
