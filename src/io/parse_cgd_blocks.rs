d62e115af9a2243bf9f479c7952e5dd428a076d8
odf <olaf.delgado@gmail.com>
2023-06-17 18:48:33 +1000

    continue work on cgd block parsing

--- src/io/parse_cgd_blocks.rs
+++ src/io/parse_cgd_blocks.rs
@@ -1,6 +1,6 @@
 use std::io::{BufRead, Read, BufReader};
 
-use crate::io::parse_cgd_line::Field;
+use super::parse_cgd_line::{parse_cgd_line, Field};
 
 
 pub enum Note {
@@ -28,16 +28,41 @@ impl Block {
             notes: vec![]
         }
     }
+
+    fn add_error(&mut self, msg: &str, lineno: usize) {
+        let note = Note::Error(format!("In line {}: {:?}", lineno, msg));
+        self.notes.push(note);
+    }
+
+    fn add_warning(&mut self, msg: &str, lineno: usize) {
+        let note = Note::Warning(format!("In line {}: {:?}", lineno, msg));
+        self.notes.push(note);
+    }
 }
 
 
 pub fn parse_blocks<T: Read>(input: T) -> Vec<Block> {
-    let mut blocks = vec![];
     let mut current_block = Block::new();
+    let mut blocks = vec![];
+    let mut current_key: Option<String> = None;
     let mut in_block = false;
 
     for (lineno, line) in BufReader::new(input).lines().enumerate() {
-
+        match line {
+            Err(e) => current_block.add_error(&format!("{:?}", e), lineno),
+            Ok(s) => match parse_cgd_line(&s) {
+                Err(e) => current_block.add_error(&e, lineno),
+                Ok((rest, fields)) => {
+                    if rest.len() > 0 {
+                        let msg = format!("unparsed line ending '{:?}'", rest);
+                        current_block.add_warning(&msg, lineno);
+                    }
+                    if fields.len() == 0 {
+                        continue
+                    }
+                }
+            }
+        }
     }
 
     blocks
